82ea57281b57bf128e72c479dcbac0c695c4cf4a
Kihyun <dlrlgus65@nate.com>
2022-06-15 20:01:47 +0900

    first commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Kihyun Lee on 2022/04/28.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

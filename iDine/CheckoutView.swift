0d4b33824f378d48dbeba9da7583ab9b28fe3d7e
looloolalaa <dlrlgus65@nate.com>
2022-07-08 22:04:28 +0900

    Add CheckoutView with Binding

---
+++ iDine/CheckoutView.swift
@@ -0,0 +1,64 @@
+//
+//  CheckoutView.swift
+//  iDine
+//
+//  Created by Kihyun Lee on 2022/07/08.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @EnvironmentObject var order: Order
+    @State private var paymentType = "Cash"
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
+    @State private var tipAmount = 15
+    
+    let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
+    let tipAmounts = [10, 15, 20, 25, 0]
+    
+    var body: some View {
+        Form {
+            Section {
+                Picker("How do you want to pay?", selection: $paymentType) {
+                    ForEach(paymentTypes, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                Toggle("Add iDine loyalty card", isOn: $addLoyaltyDetails.animation())
+                
+                if addLoyaltyDetails {
+                    TextField("Enter your iDine ID", text: $loyaltyNumber)
+                }
+            }
+            
+            Section(header: Text("Add a tip?")) {
+                Picker("Percentage:", selection: $tipAmount) {
+                    ForEach(tipAmounts, id: \.self) {
+                        Text("\($0)%")
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            
+            Section(header: Text("Total: $100")) {
+                Button("Confirm order") {
+                    //action
+                }
+            }
+        }
+        .navigationTitle("Payment")
+        .navigationBarTitleDisplayMode(.inline)
+        
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            CheckoutView()
+                .environmentObject(Order())
+        }
+    }
+}

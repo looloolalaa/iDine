f377333c32127ae326021d4dadc0f1b9663379c7
looloolalaa <dlrlgus65@nate.com>
2022-07-19 18:56:11 +0900

    iDine done

--- iDine/ContentView.swift
+++ iDine/ContentView.swift
@@ -50,7 +50,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .previewInterfaceOrientation(.portrait)
             .environmentObject(Bookmark())
     }
 }
